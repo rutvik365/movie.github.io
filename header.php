082244220c59261f0e477953cfef39a2c85256ce
rutvik365 <rutvikpramar962@gmail.com>
2021-10-26 19:00:23 -0700

    movei website
    i am happy

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./css/bootstrap.css">
+  <script src="./js/jquery.js"></script>
+  <script src="./js/bootstrap.js"></script>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,76 @@
+<?php require "header.php" ?>
+
+<div class="container">
+  <div class="jumbotron">
+    <h1 align="center">WEBSITE NAME</h1>
+    <p align="center">Line Tag</p>
+  </div>
+</div>
+<div class="container">
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+</div>
+<div class="container">
+<div class="col-sm-9">
+  <div class="row">
+   <div class="col-md-3">
+   <img src="img10.jpg" class="img-responsive">
+   </div>
+   <div class="col-md-9">
+   <p align="justify">you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.</p>
+   </div>
+</div>
+<br/>
+<!--<button type="button"  href="#" class="btn btn-default">Read more</button>-->
+<a target="_blank" href="#" class="btn btn-default">Read More »</a>
+
+<hr/>
+
+<div class="row">
+   <div class="col-md-3">
+   <img src="img10.jpg" class="img-responsive">
+   </div>
+   <div class="col-md-9">
+   <p align="justify">you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.</p>
+   </div>
+</div>
+<br/>
+<a target="_blank" href="#" class="btn btn-default">Read More »</a>
+
+<hr/>
+
+<div class="row">
+   <div class="col-md-3">
+   <img src="img10.jpg" class="img-responsive">
+   </div>
+   <div class="col-md-9">
+   <p align="justify">you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.</p>
+   </div>
+</div>
+<br/>
+<a target="_blank" href="#" class="btn btn-default">Read More »</a>
+
+<hr/>
+
+<div class="row">
+   <div class="col-md-3">
+   <img src="img10.jpg" class="img-responsive">
+   </div>
+   <div class="col-md-9">
+   <p align="justify">you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or at your option any later version.</p>
+   </div>
+</div>
+<br/>
+<a target="_blank" href="#" class="btn btn-default">Read More »</a>
+</div>
+<?php require "footer.php" ?>
